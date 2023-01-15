fe91a6b83e9fe770fab028c8d2f62ca5761aee67
samfernandes03 <samuel.pinho.fernandes@gmail.com>
2023-01-15 01:55:59 +0000

    validations and minor fixes

---
+++ scriptlogin.js
@@ -0,0 +1,60 @@
+function onChangeEmail() {
+    toogleButtonsDisable();
+    toogleEmailErrors();
+}
+
+function onChangePassword() {
+    toogleButtonsDisable();
+    tooglePasswordErrors();
+}
+
+function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function toogleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+}
+
+function tooglePasswordErrors() {
+    const password = form.password().value;
+
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toogleButtonsDisable() {
+    const emailValid = isEmailValid();
+    form.recoverPassword().disabled=!emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+    const password = form.password().value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+const form = {
+    email: () => document.getElementById("email"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+    loginButton: () => document.getElementById("login-button"),
+    password: () => document.getElementById("password"),
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    recoverPassword: () => document.getElementById("recover-password-button")
+}
