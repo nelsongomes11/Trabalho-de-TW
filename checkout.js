50ee7d5ca1c2d07a9259d99c358964ee9085e92e
Nelson Gomes <nelson.gomes22@hotmail.com>
2023-01-20 12:08:29 +0000

    adicionei js no botão

---
+++ checkout.js
@@ -0,0 +1,6 @@
+let button =document.getElementById("checkbutton");
+button.addEventListener("click",fcheck);
+
+function fcheck(){
+    alert("Thanks for your purchase! A receipt will be sent to your e-mail!")
+}
